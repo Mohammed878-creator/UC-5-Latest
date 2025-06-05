e8c74291396a70e33a6404f9403735286125d8db
Mohammed878-creator <mdshaik878@gmail.com>
2025-06-05 15:35:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,21 +13,6 @@ terraform {
   required_version = ">= 1.10.0"
 }
 
-
-
-# Main Terraform configuration file
-
-terraform {
-  required_version = ">= 1.12.0"
-  
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 provider "aws" {
   region = var.aws_region
   
